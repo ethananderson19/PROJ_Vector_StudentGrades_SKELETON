f18c42b8ef0fc839b2c9b0d7a53308bea5d4e9e9
Ethan Anderson <ethan.anderson.19@cnu.edu>
2021-10-02 13:34:44 -0400

    first commit

--- utilities/utilities.cpp
+++ utilities/utilities.cpp
@@ -34,4 +34,80 @@ int stringToInt(const char *myString) {
 double stringToDouble(const char *myString) {
 	return atof(myString);
 }
+/***
+ * Clears allStudentData, Opens file, For each line; parses name, midterm1,midterm2 and an
+ * optional finalgrade (if present in file)from that line. Puts this info into a studentData struct,
+ * adds this struct to allstudentData vector.
+ * NOTE:
+ * Ignore empty lines
+ * Non empty lines will contain 4 or 5 values to be parsed.
+ * the default param separator_char is defined in the header file declaration of readFile ONLY, it is not in the readFile definition.
+ * @param file - where student data is stored
+ * @param allstudentData - a vector that holds the student data info after it has been read from file
+ * @param separator_char - the character that separates each bit of data in a line of student data
+ * @return COULD_NOT_OPEN_FILE
+ *         SUCCESS
+ */
+int readFile(std::string &file, std::vector<KP::studentData> &allstudentData, char separator_char=KP::SEPERATOR_CHAR ){
+
+
+/***
+ * Iterates over all students in vector, and averages midterm1 and midterm2 to get the finalgrade
+ * @param allstudentData
+ * @return VECTOR_CONTAINS_NO_STUDENTS
+ *         SUCCESS
+ */
+	allstudentData.clear();
+	std::string line;
+	std::string token;
+	KP::studentData myStudentData;
+	stringstream ss;
+	ifstream myInputfile;
+	myInputfile.open(file.c_str(), ios::in);
+	if (!myInputfile.is_open())
+			return false;
+	while (!myInputfile.eof()) {
+		std::getline(myInputfile, line);
+				ss.str(line);
+		std::getline(ss, myStudentData.name, separator_char);
+		std::getline(ss, token, separator_char);
+		myStudentData.midterm1=stringToInt(token.c_str());
+		std::getline(ss, token, separator_char);
+		myStudentData.midterm2=stringToInt(token.c_str());
+		allstudentData.push_back(myStudentData);
+	}
+	myInputfile.close
+	return KP::SUCCESS;
+
+
+
+
+}
+int calculateFinalGrade(std::vector<KP::studentData> &allstudentData);
+//void extractFailingStudents(double failgrade = FAILGRADE);
+
+/***
+ * Opens file, serializes data from allstudentData to file, closes file
+ * the default param seperator_char is defined in the header file ONLY
+ * @param file - where student data is stored
+ * @param allstudentData - a vector that holds student data info that will be written to file
+ * @param separator_char - the character that separates each bit of data in a line of student data
+ * @return VECTOR_CONTAINS_NO_STUDENTS
+ *         COULD_NOT_OPEN_FILE
+ *         SUCCESS
+ */
+int writeFile(std::string &file, std::vector<KP::studentData> &allstudentData, char separator_char=KP::SEPERATOR_CHAR );
+
+//sorts studentdata based on SORT_TYPE
+/***
+ *
+ * @param allstudentData - a vector that holds student data info that will be written to file
+ * @param st - how to sort the vector
+ * @return VECTOR_CONTAINS_NO_STUDENTS
+ *         SUCCESS
+ */
+int sortStudentData(std::vector<KP::studentData> &allstudentData,KP::SORT_TYPE st);
+
+//if myString does not contain a string rep of number returns 0
+//if int not large enough has undefined behaviour, very fragile
 
